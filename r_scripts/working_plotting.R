897770c44990bfaf37e3fc75007a7611d2dd28a4
heatherhops2it <heathergallowaybda@gmail.com>
2024-11-01 16:23:32 -0400

    manipulating data

--- r_scripts/working_plotting.R
+++ r_scripts/working_plotting.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(janitor)
+library(rstatix)  # install.packages("rstatix")
 
 
 # Load in data ------------------------------------------------------------
@@ -14,10 +15,11 @@ s04_raw <- read_csv("datasets/S1123KIP1_048K_S04_S7112_20240108_070004.Table.1.s
 
 # Combining datasets ------------------------------------------------------
 
-df_combined <- bind_rows(s02_raw, s04_raw)
+kipukas_raw <- bind_rows(s02_raw, s04_raw)
 
-df_cleaned <- df_combined |> 
+kipukas_clean <- kipukas_raw |> 
   clean_names() |> 
+  mutate(center_time_percent = (center_time_s - time_5_percent_s)/dur_90_percent_s) |> 
   select(
     site, 
     type, 
@@ -25,9 +27,23 @@ df_cleaned <- df_combined |>
     dur_90_percent_s,
     peak_freq_hz,
     syllables_p_song,
-    center_time_s)
+    center_time_percent) |> 
+  filter(!is.na(type))
+
+songs <- kipukas_clean |> 
+  filter(type == "Song")
+
+syllables <- kipukas_clean |> 
+  filter(type == "Syllable") |> 
+  select(!syllables_p_song)
 
 
-# Plot creation -----------------------------------------------------------
+
+# Pivot tables for stats --------------------------------------------------
+
+s
+
+
+
 
 
