977e001fa706d21aee8ca7e82ebb7056ba1d4fb2
heatherhops2it <heathergallowaybda@gmail.com>
2024-11-04 13:21:38 -0500

    ran stats and made plots

--- r_scripts/working_plotting.R
+++ r_scripts/working_plotting.R
@@ -19,7 +19,8 @@ kipukas_raw <- bind_rows(s02_raw, s04_raw)
 
 kipukas_clean <- kipukas_raw |> 
   clean_names() |> 
-  mutate(center_time_percent = (center_time_s - time_5_percent_s)/dur_90_percent_s) |> 
+  mutate(
+    center_time_percent = (center_time_s - time_5_percent_s)/dur_90_percent_s) |>
   select(
     site, 
     type, 
@@ -39,11 +40,97 @@ syllables <- kipukas_clean |>
 
 
 
-# Pivot tables for stats --------------------------------------------------
+# Pivoting tables for stats -----------------------------------------------
 
-s
+songs_long <- songs |> 
+  pivot_longer(
+    c(
+      "freq_95_percent_hz", 
+      "dur_90_percent_s",
+      "peak_freq_hz",
+      "syllables_p_song",
+      "center_time_percent"),
+    names_to = "variable", 
+    values_to = "value")
+
+syllables_long <- syllables |> 
+  pivot_longer(
+    c(
+      "freq_95_percent_hz", 
+      "dur_90_percent_s",
+      "peak_freq_hz",
+      "center_time_percent"),
+    names_to = "variable", 
+    values_to = "value")
+
+
+
+# Simple t-test -----------------------------------------------------------
+
+songs_long |> 
+  group_by(variable) |> 
+  t_test(value ~ site)
+
+syllables_long |> 
+  group_by(variable) |> 
+  t_test(value ~ site)
 
 
 
 
+# Mean and standard deviation ---------------------------------------------
 
+songs_long |> 
+  group_by(variable) |> 
+  summarise(
+    mean= mean(value), 
+    sd= sd(value), 
+    max = max(value),
+    min = min(value))
+
+syllables_long |> 
+  group_by(variable) |> 
+  summarise(
+    mean= mean(value), 
+    sd= sd(value), 
+    max = max(value),
+    min = min(value))
+
+
+
+# Chi square --------------------------------------------------------------
+
+songs |> 
+  select(site, syllables_p_song) |> 
+  chisq_test()
+
+
+
+# Box and whisker plots ---------------------------------------------------
+
+songs_long |> 
+  ggplot(aes(x = value)) +
+  geom_density(aes(fill = site, colour = site), alpha = 0.5) +
+  facet_wrap(
+    ~variable,
+    scales = "free",
+    labeller = as_labeller(c(
+      `center_time_percent` = "Center Time (%)",
+      `dur_90_percent_s` = "90% Duration (s)",
+      `freq_95_percent_hz` = "95% Frequency (Hz)",
+      `peak_freq_hz` = "Peak Frequency (Hz)",
+      `syllables_p_song` = "Syllables Per Song")))
+
+
+syllables_long |> 
+  ggplot(aes(x = value)) +
+  geom_density(aes(fill = site, colour = site), alpha = 0.5) +
+  facet_wrap(
+    ~variable,
+    scales = "free",
+    labeller = as_labeller(c(
+      `center_time_percent` = "Center Time (%)",
+      `dur_90_percent_s` = "90% Duration (s)",
+      `freq_95_percent_hz` = "95% Frequency (Hz)",
+      `peak_freq_hz` = "Peak Frequency (Hz)")))
+  
